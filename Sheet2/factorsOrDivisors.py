000e70a12a6200e9b203fdd0e8437058d889b656
Harshu1006 <bhardwajharsh2244@gmail.com>
2025-08-21 10:27:18 +0530

    Update factorsOrDivisors.py

--- Sheet2/factorsOrDivisors.py
+++ Sheet2/factorsOrDivisors.py
@@ -1,10 +1,6 @@
 num = int(input("Enter number: "))
 power = int(input("Enter the power: "))
-# output = num
-# for i in range(1, power):
-#     # print(num)
-#     output = output * num
-# print(f"{num} power {power} is: {output}")
+
 
 result = num**power
-print(f"{num} power {power} is: {result}")
+print("num power power is:",result)
