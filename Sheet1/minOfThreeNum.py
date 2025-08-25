551d3913a2fac9cc41c7f6bdb7ca141bf25a2883
Harshu1006 <bhardwajharsh2244@gmail.com>
2025-08-25 11:57:11 +0530

    Update minOfThreeNum.py

--- Sheet1/minOfThreeNum.py
+++ Sheet1/minOfThreeNum.py
@@ -2,9 +2,9 @@ a = int(input("Enter number a: "))
 b = int(input("Enter number b: "))
 c = int(input("Enter number c: "))
 
-if a > b and a > c:
-    print("a is the greatest")
-elif b > c:
-    print("b is the greatest")
+if a < b and a < c:
+    print("a is the smallest")
+elif b <a && b <c:
+    print("b is the smallest")
 else:
-    print("c is the greatest")
+    print("c is the smallest")
