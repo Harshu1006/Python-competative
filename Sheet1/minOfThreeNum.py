7d351319b42fd49cf35ed11bec6d1448d4be270e
Harsh <bhardwajharsh2244@gmail.com>
2025-08-25 11:12:17 +0530

    +

---
+++ Sheet1/minOfThreeNum.py
@@ -0,0 +1,10 @@
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+c = int(input("Enter number c: "))
+
+if a < b and a < c:
+    print("a is the smallest")
+elif b < c:
+    print("b is the smallest")
+else:
+    print("c is the smallest")
--- Sheet2/power.py
+++ Sheet2/power.py
@@ -1,4 +1,11 @@
-A = int(input("Enter the base (A): "))
-B = int(input("Enter the exponent (B): "))
-result = A ** B
-print("The value of", A, "^", B, "is", result)
+# A = int(input("Enter the base (A): "))
+# B = int(input("Enter the exponent (B): "))
+# result = A ** B
+# print("The value of", A, "^", B, "is", result)
+ #2- method
+a = int(input("a:"))
+b = int(input("b:"))
+ans=1
+for i in range(b):
+    ans = ans*b
+    print(ans)
